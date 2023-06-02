d7260bb25accea9f3a4877616162e2b6329b5caf
Ocyplex <Ocyplex@wp.pl>
2023-06-02 10:30:10 +0200

    Initial commit

---
+++ WebApplication/Models/JobOffer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class JobOffer
+    {
+        public int id { get; set; }
+
+        public string jobTitle { get; set; }
+
+        public string jobLocation { get; set; }
+
+        public string description { get; set; }
+
+        public string salary { get; set; }
+
+        public DateTime startDate { get; set; }
+    }
+}
